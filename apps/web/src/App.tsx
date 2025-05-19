883eb9e9cdfd45a6abb722c2bbea49ca7bdacba0
peezy <itspeezy@protonmail.com>
2025-05-19 17:18:58 -0300

    back to pools

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -40,6 +40,7 @@ function Forms() {
 
 
 function App() {
+  return <Forms/>
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
