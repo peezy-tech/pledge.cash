e32763b20392532149f9127318e6b27fa72bcaf7
peezy <itspeezy@protonmail.com>
2025-05-27 14:34:10 -0300

    location.origin as api url

--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -4,7 +4,7 @@ import type { App } from 'api'
 // API configuration
 export const API_BASE_URL = location.origin === "http://localhost:5173" ? "http://localhost:3000" : location.origin;
 
-export const api = edenTreaty<App>(API_BASE_URL, { $fetch: { credentials: 'include' } })
+export const api = edenTreaty<App>(location.origin, { $fetch: { credentials: 'include' } })
 
 console.log(API_BASE_URL);
 
